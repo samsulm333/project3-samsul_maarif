fdea5a16963967a1b4fcccd5ff8310548e392f75
samsul <samsul.m333@gmail.com>
2026-02-28 16:58:42 +0700

    trial deploy gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/project3-samsul_maarif/",
   plugins: [tailwindcss()],
 });
